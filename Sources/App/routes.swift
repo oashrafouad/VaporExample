ea9c8214987d63633d60b406cd9e5520ac047c71
Omar Ashraf <oashrafouad@gmail.com>
2025-02-01 02:26:22 +0200

    update

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,15 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+
+    app.get("example") { req async -> String in
+
+        "This is an example"
+    }
+    // app.get("hello", ":name") { req async -> String in
+    //     guard let name = req.parameters.get("name") else {
+    //         throw Abort(.badRequest)
+    //     }
+    //     return "Hello, \(name)!"
+    // }
 }
